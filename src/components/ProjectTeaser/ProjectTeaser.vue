3a1f184eeb67872b13de1f75da5774f09a4bdafb
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-03-03 08:43:45 +0100

    added Project teaser

---
+++ src/components/ProjectTeaser/ProjectTeaser.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ProjectTeaser",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<template>
+  <div id="projectTeaser" class="bg-secondary padding-top-5 padding-bottom-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-md-6">
+          <img
+            src="../../assets/components/titleBackground.jpg"
+            alt="Team Bild"
+            class="width-80 margin-left-10 radius-16"
+          />
+        </div>
+        <div class="col-xs-12 col-md-6 flex column justify-center height-100">
+          <h2>Projekte</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
+          </p>
+          <button class="button-primary width-10 margin-top-4">Details</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped src="./ProjectTeaser.scss"></style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,6 +3,7 @@ import { defineComponent } from "vue";
 import titleBackground from "../../components/TitleBackground/TitleBackground.vue";
 import contactForm from "../../components/ContactForm/ContactForm.vue";
 import aboutUs from "../../components/AboutUs/AboutUs.vue";
+import projectTeaser from "../../components/ProjectTeaser/ProjectTeaser.vue";
 
 export default defineComponent({
   setup() {},
@@ -10,15 +11,25 @@ export default defineComponent({
     titleBackground,
     contactForm,
     aboutUs,
+    projectTeaser,
   },
 });
 </script>
 
 <template>
   <div id="home">
-    <titleBackground></titleBackground>
-    <h1>Home</h1>
-    <aboutUs />
+    <div class="padding-bottom-5">
+      <titleBackground></titleBackground>
+    </div>
+
+    <div class="margin-top-5 padding-bottom-5">
+      <aboutUs />
+    </div>
+
+    <div class="margin-top-5 padding-bottom-5">
+      <projectTeaser />
+    </div>
+
     <contactForm />
   </div>
 </template>
