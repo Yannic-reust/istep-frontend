94e5c89879c1aa488e2b4f8670052cda2817818a
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-07-22 17:42:50 +0200

    query team working

--- src/components/Team/Team.vue
+++ src/components/Team/Team.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="team">
     <h1>Team</h1>
-    <div class="container">
+    <div class="container" v-if="result">
       <div class="row">
-        <div class="col-lg-3 col-md-6 col-xs-12" v-for="(teamMember, index) in teamMembers" :key="index">
-          <TeamMember :name="teamMember.name" :text="teamMember.text"/>
+        <div
+          class="col-lg-3 col-md-6 col-xs-12"
+          v-for="(teamMember, index) in result.teamMembers.data"
+          :key="index"
+        >
+        <p></p>
+          <TeamMember
+            :name="teamMember.attributes.Name"
+            :text="teamMember.attributes.Text"
+          />
         </div>
       </div>
     </div>
@@ -14,39 +22,68 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import TeamMember from "./TeamMember/TeamMember.vue";
+import gql from "graphql-tag";
+import { useQuery } from "@vue/apollo-composable";
 export default defineComponent({
   name: "Team",
-  data() {
-  return{
-    teamMembers:[
-      {
-        name:"Adrian",
-        text:"Text Adrian"
-      },
-      {
-        name:"Matt",
-        text:"Text Matt"
-      },
-      {
-        name:"Dominic",
-        text:"Text Dominic"
-      },
-      {
-        name:"Aline",
-        text:"Text Aline"
-      },
-      {
-        name:"Yannic",
-        text:"Text Yannic"
-      }
-    ]
-  }
-  
+  setup() {
+    const { result, loading, error } = useQuery(teamMembers_QUERY);
+    return {
+      result,
+      loading,
+      error,
+    };
   },
-  components:{
+  data() {
+    return {
+      teamMembers: [
+        {
+          name: "Adrian",
+          text: "Text Adrian",
+        },
+        {
+          name: "Matt",
+          text: "Text Matt",
+        },
+        {
+          name: "Dominic",
+          text: "Text Dominic",
+        },
+        {
+          name: "Aline",
+          text: "Text Aline",
+        },
+        {
+          name: "Yannic",
+          text: "Text Yannic",
+        },
+      ],
+    };
+  },
+  components: {
     TeamMember,
-  }
+  },
 });
+
+const teamMembers_QUERY = gql`
+  query teamMembers {
+    teamMembers {
+      data {
+        attributes {
+          Name
+          Text
+          Image {
+            data {
+              attributes {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 </script>
 
 <style scoped src="./Team.scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-import { createApp } from 'vue'
-
-import './App.scss'
-//@ts-ignore
+import { createApp, provide, h } from 'vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import App from './App.vue'
 //@ts-ignore
-import router from './router.js'
 
-createApp(App).use(router).mount('#app')
+import router from './router.js'
+import './App.scss'
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'https://istep-backend.blackbox-mit.ch/graphql',
+})
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+app.use(router).mount('#app');
