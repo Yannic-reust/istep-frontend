32b6cffcf47d6c1b025e7a628fabee7f92753583
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-07-23 19:16:35 +0200

    wip contact form

--- src/components/ContactForm/ContactForm.vue
+++ src/components/ContactForm/ContactForm.vue
@@ -1,37 +1,65 @@
 <template>
   <div id="contactForm" class="margin-top-5 margin-bottom-5">
-    <form ref="form" id="myForm" @submit.prevent="sendEmail">
-      <div class="container">
-        <h2 class="primary text-center">Kontaktiere uns!</h2>
-        <div class="row margin-top-2">
-          <div class="col-sm-12 col-md-6 margin-top-2">
-            <label class="primary text-h4">Dein Name</label>
-            <input type="text" name="user_name" class="input-text" required />
+    <h2 class="primary text-center">Kontaktiere uns!</h2>
+
+    <div >
+      <form ref="form" id="myForm" @submit.prevent="sendEmail">
+        <div class="container">
+        
+          <div class="row margin-top-2">
+            <div class="col-sm-12 col-md-6 margin-top-2">
+              <label class="primary text-h4">Dein Name</label>
+              <input type="text" name="user_name" class="input-text" required />
+            </div>
+            <div class="col-sm-12 col-md-6 margin-top-2">
+              <label class="primary text-h4">Deine Email Adresse</label>
+              <input
+                type="email"
+                name="user_email"
+                class="input-text"
+                required
+              />
+            </div>
           </div>
-          <div class="col-sm-12 col-md-6 margin-top-2">
-            <label class="primary text-h4">Deine Email Adresse</label>
-            <input type="email" name="user_email" class="input-text" required />
+
+          <div class="row">
+            <div class="col-xs-12 margin-top-2 marginbottom-2">
+              <p class="text-h4">Warum möchtest du mit uns in kontakt treten?</p>
+            </div>
+         
+          <div class="col-xs-12 col-md-4 flex">
+            <input type="radio" id="goenner" name="radio_input" value="30" />
+            <label for="goenner"><p class="margin-left-2 text-h4">Gönner</p></label><br />
+          </div>
+          <div class="col-xs-12 col-md-4 flex">
+            <input type="radio" id="helfer" name="radio_input" value="30" />
+            <label for="helfer"><p class="margin-left-2 text-h4">Helfer</p></label><br />
+          </div>
+          <div class="col-xs-12 col-md-4 flex">
+            <input type="radio" id="kurs_interessenten" name="radio_input" value="30" />
+            <label for="kurs_interessenten"><p class="margin-left-2 text-h4">Kurs Interessierte</p></label><br />
           </div>
         </div>
-        <div class="row">
-          <div class="col-sm-12 padding-top-2">
-            <label class="primary text-h4">Deine Nachricht</label>
-            <textarea
-              name="message"
-              class="input-text textarea padding-2"
-              required
-            ></textarea>
+          <div class="row">
+            <div class="col-sm-12 padding-top-2">
+              <label class="primary text-h4">Deine Nachricht</label>
+              <textarea
+                name="message"
+                class="input-text textarea padding-2"
+                required
+              ></textarea>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="flex-center">
-        <input
-          type="submit"
-          value="Nachricht Senden"
-          class="button-primary margin-top-4 button"
-        />
-      </div>
-    </form>
+        <div class="flex-center">
+          <input
+            type="submit"
+            value="Nachricht Senden"
+            class="button-primary margin-top-4 button"
+          />
+        </div>
+      </form>
+    </div>
 
     <div class="container margin-top-5" v-if="success">
       <h5 class="success text-center">Deine Nachricht wurde gesendet!</h5>
@@ -55,6 +83,7 @@ export default defineComponent({
     return {
       success: false,
       fail: false,
+      openForm: false,
     };
   },
   methods: {
