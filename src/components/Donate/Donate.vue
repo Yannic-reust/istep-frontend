f81f75fdbb14da34e6477ed7c01fb8f49da0e20a
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-08-03 14:48:30 +0200

    background add

--- src/components/Donate/Donate.vue
+++ src/components/Donate/Donate.vue
@@ -14,19 +14,19 @@ export default defineComponent({
 </script>
 
 <template>
-  <div id="donate">
+  <div id="donate" class="bg-secondary padding-10">
     <div class="container">
-      <div class="row">
+      <div class="row equal">
        
-        <div class="col-xs-12 col-md-6">
+        <div class="col-xs-12 col-md-6 margin-bottom-5">
           <img
             :src="`https://istep-backend.blackbox-mit.ch${donate.Image.data.attributes.url}`"
             alt="Team Bild"
-            class="img radius-16"
+            class="img radius-16 "
           />
         </div>
-        <div class="col-xs-12 col-md-6">
-          <h2>{{ donate.Title }}</h2>
+        <div class="col-xs-12 col-md-6 flex justify-center column">
+          <h2 class="margin-bottom-2">{{ donate.Title }}</h2>
           <p>{{ donate.Text }}</p>
           <Icon icon="ep:arrow-up-bold" :rotate="2" width="32px" />
         </div>
