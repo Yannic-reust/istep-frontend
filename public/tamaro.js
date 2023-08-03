9a47d0a4804e244b5a91879cd956d4a873efb0d3
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-08-03 18:15:43 +0200

    wip donate

---
+++ public/tamaro.js
@@ -0,0 +1,6 @@
+  window.rnw.tamaro.runWidget('.rnw-widget-container', {language: 'en'})
+
+
+
+
+
--- src/components/Donate/Donate.vue
+++ src/components/Donate/Donate.vue
@@ -1,36 +1,57 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent,ref } from "vue";
 import { Icon } from "@iconify/vue";
 export default defineComponent({
   name: "Donate",
   props: ["donate"],
   setup() {
-    return {};
+    const open = ref(false);
+    return {
+      open
+    };
   },
   components: {
     Icon,
+  },
+  beforecreated() {
+    var scripts = [
+     
+    "https://tamaro.raisenow.com/istep-b7da/latest/widget.js",
+      "./tamaro.js",
+    ];
+    scripts.forEach((script) => {
+      let tag = document.createElement("script");
+      tag.setAttribute("src", script);
+      document.head.appendChild(tag);
+    });
   },
 });
 </script>
 
 <template>
-  <div id="donate" class="bg-secondary padding-10">
-    <div class="container">
-      <div class="row equal">
-       
-        <div class="col-xs-12 col-md-6 margin-bottom-5">
-          <img
-            :src="`https://istep-backend.blackbox-mit.ch${donate.Image.data.attributes.url}`"
-            alt="Team Bild"
-            class="img radius-16 "
-          />
-        </div>
-        <div class="col-xs-12 col-md-6 flex justify-center column">
-          <h2 class="margin-bottom-2">{{ donate.Title }}</h2>
-          <p>{{ donate.Text }}</p>
-          <Icon icon="ep:arrow-up-bold" :rotate="2" width="32px" />
+  <div>
+    <div id="donate" class="bg-secondary padding-10">
+      <div class="container">
+        <div class="row equal">
+          <div class="col-xs-12 col-md-6 ">
+            <img
+              :src="`https://istep-backend.blackbox-mit.ch${donate.Image.data.attributes.url}`"
+              alt="Team Bild"
+              class="img radius-16"
+            />
+          </div>
+          <div class="col-xs-12 col-md-6 flex justify-center column margin-top-5 margin-bottom-5">
+            <h2 class="">{{ donate.Title }}</h2>
+            <p>{{ donate.Text }}</p>
+            
+            <button class="button-primary width-50 margin-top-2" @click="open = !open">Jetzt Spenden</button>
+            <!-- <Icon icon="ep:arrow-up-bold" :rotate="2" width="32px" /> -->
+          </div>
         </div>
       </div>
+    </div>
+    <div class="container margin-top-5" >
+      <div class="rnw-widget-container width-100"></div>
     </div>
   </div>
 </template>
