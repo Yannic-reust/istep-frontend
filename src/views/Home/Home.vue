61f7b7d0a8dadcabde1ccb57aaba4657b634c0b7
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-08-21 11:14:58 +0200

    wip

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -66,7 +66,6 @@ export default defineComponent({
               Project {
                 Title
                 Text
-       
                 ProjectDetailBox {
                   Title
                   Text
