4edfa8366580c34adb0f977edeb834982603688f
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-08-21 11:13:48 +0200

    wip

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -29,7 +29,7 @@ export default defineComponent({
     <div class="navContainer flex space-between height-100">
       <div class="width-20 margin-left-5 flex align-center">
         <router-link to="/" class="none">
-          <h2 class="secondary ">iSTEP</h2>
+          <h2 class="secondary">iSTEP</h2>
         </router-link>
         <p class="secondary margin-left-1">into my future</p>
       
