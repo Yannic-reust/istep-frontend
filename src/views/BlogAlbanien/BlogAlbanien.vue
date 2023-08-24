c0e197968c4a8fba7cb4df271b3ded28cfdcd1c5
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-08-24 16:52:29 +0200

    new blog secion

--- src/views/BlogAlbanien/BlogAlbanien.vue
+++ src/views/BlogAlbanien/BlogAlbanien.vue
@@ -58,7 +58,7 @@ export default {
               Title
               Teaser
               Date
-              Text
+              
               Image {
                 data {
                   attributes {
--- src/views/BlogPostAlbanien/BlogPostAlbanien.vue
+++ src/views/BlogPostAlbanien/BlogPostAlbanien.vue
@@ -2,27 +2,40 @@
   <div id="blogPostAlbanien" v-if="result" class="margin-top-5">
     <div class="container margin-top-5">
       <div class="row">
-        <div class="col-md-6 col-xs-12">
+        <div class="col-md-6 col-xs-12 margin-top-4">
           <div class="flex space-between align-bottom">
             <h2>{{ result.blogAlbanien.data.attributes.Title }}</h2>
             <p>{{ result.blogAlbanien.data.attributes.Date }}</p>
           </div>
           <p class="text-h5 margin-top-1">
-        {{ result.blogAlbanien.data.attributes.Teaser }}
-      </p>
+            {{ result.blogAlbanien.data.attributes.Teaser }}
+          </p>
         </div>
-        <div class="col-md-6 col-xs-12">
+        <div class="col-md-6 col-xs-12 margin-top-4">
           <img
             :src="`https://istep-backend.blackbox-mit.ch${result.blogAlbanien.data.attributes.Image.data.attributes.url}`"
             alt=""
-            class="width-80 margin-left-10 radius-8"
+            class="xs-width-80 width-100 xs-margin-left-10 radius-8"
           />
         </div>
       </div>
-
-     
-      <div class="margin-top-2">
-        <Markdown :source="result.blogAlbanien.data.attributes.Text" />
+      <div class=" margin-bottom-4">
+        <div
+          class="row margin-top-4"
+          v-for="(item, index) in result.blogAlbanien.data.attributes.Section"
+          :key="index"
+        >
+          <div class="col-xs-12 col-md-6">
+            <Markdown :source="item.Text" />
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <img
+              :src="`https://istep-backend.blackbox-mit.ch${item.Image.data.attributes.url}`"
+              alt=""
+              class="xs-width-80 width-100 xs-margin-left-10 radius-8"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -61,7 +74,16 @@ export default {
               Title
               Teaser
               Date
-              Text
+              Section {
+                Text
+                Image {
+                  data {
+                    attributes {
+                      url
+                    }
+                  }
+                }
+              }
 
               Image {
                 data {
